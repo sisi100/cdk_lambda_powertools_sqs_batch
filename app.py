6ce260b48e18faf4f8e2a44bddec27d0c1eae63b
Shun Isii <sisii00.swork@gmail.com>
2021-09-05 14:42:56 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from cdk_lambda_powertools_sqs_batch.cdk_lambda_powertools_sqs_batch_stack import CdkLambdaPowertoolsSqsBatchStack
+
+
+app = core.App()
+CdkLambdaPowertoolsSqsBatchStack(app, "cdk-lambda-powertools-sqs-batch")
+
+app.synth()
